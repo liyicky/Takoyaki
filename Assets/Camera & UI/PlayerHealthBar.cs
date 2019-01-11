4bb2ebf937277ce6c9f634a56d988063429ed0b4
liyicky <liyicky@gmail.com>
2019-01-11 23:51:43 +0900

    health setup

---
+++ Assets/Camera & UI/PlayerHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasRenderer))]
+public class PlayerHealthBar : MonoBehaviour
+{
+
+    CanvasRenderer healthPoolHolder;
+    Player player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthPoolHolder = GetComponent<CanvasRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float xValue = -(player.healthAsPercentage / 2f) - 0.5f;
+        float yValue = (player.healthAsPercentage * 0.01f) * -421f;
+        healthPoolHolder.transform.localPosition = Vector3.zero;
+        // healthPoolHolder.uvRect = new Rect(healthPoolHolder.uvRect.xValue, healthPoolHolder.uvRect.xValue)
+        // healthPoolSprite.
+        // healthPoolSprite.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float maxHealthPoints = 100f;
+
+    float currentHealthPoint = 100f;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoint / maxHealthPoints;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
