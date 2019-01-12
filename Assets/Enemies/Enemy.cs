ff3bc2b56e91c8c0aa105404083e77a2381ad9f7
liyicky <liyicky@gmail.com>
2019-01-12 12:34:58 +0900

    enemy health ui

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float maxHealthPoints = 100f;
+
+    [SerializeField] float currentHealthPoint = 100f;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoint / maxHealthPoints;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
