fc5fd5cd1cdfcd67aa6e555bcee3e2639425fa00
liyicky <liyicky@gmail.com>
2019-01-16 13:51:01 +0900

    cleanup

---
+++ Assets/Weapons/Projectiles/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour
+{
+
+    // Note: Other classes can set
+    public float projectileSpeed;
+    public float damageCaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Component damageableComponent = other.gameObject.GetComponent(typeof(IDamageable));
+        if (damageableComponent)
+        {
+            (damageableComponent as IDamageable).TakeDamage(damageCaused);
+            Destroy(gameObject, 60f);
+        }
+    }
+}
