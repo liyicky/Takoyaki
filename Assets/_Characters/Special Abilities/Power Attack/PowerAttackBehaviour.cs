c1e058d5c67a1e6da66431f002fd63d6d328165a
liyicky <liyicky@gmail.com>
2019-01-21 14:44:13 +0900

    setup special abilities

---
+++ Assets/_Characters/Special Abilities/Power Attack/PowerAttackBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{    
+    public class PowerAttackBehaviour : MonoBehaviour, ISpecialAbility
+    {
+        PowerAttackConfig config;
+
+        public void SetConfig(PowerAttackConfig configToSet)
+        {
+            this.config = configToSet;
+        }
+
+        public void Use()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
