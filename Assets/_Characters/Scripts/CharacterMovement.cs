9f674c193aac24edf3fdf3a0e0221ab1904ffa44
liyicky <liyicky@gmail.com>
2019-01-23 16:15:24 +0900

    refactor ThirdPersonCharacter into CharacterMovement

--- Assets/_Characters/Scripts/CharacterMovement.cs
+++ Assets/_Characters/Scripts/CharacterMovement.cs
@@ -1,25 +1,29 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.Characters.ThirdPerson;
 using UnityEngine.AI;
 using RPG.CameraUI;
 
 namespace RPG.Characters
   {
   [RequireComponent(typeof (NavMeshAgent))]
-  [RequireComponent(typeof (ThirdPersonCharacter))]
   [RequireComponent(typeof(CameraRaycaster))]
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(Animator))]
 
   public class CharacterMovement : MonoBehaviour
   {
     [SerializeField] float stoppingDistance = 1f;
     [SerializeField] float moveSpeedMultiplier = 1f;
+    [SerializeField] float animSpeedMultiplier = 1f;
+    [SerializeField] float moveThreshold = 1f;
 
-    ThirdPersonCharacter character;   // A reference to the ThirdPersonCharacter on the object
-    GameObject walkTarget;
     NavMeshAgent agent;
     Animator animator;
     Rigidbody rigidbody;
+    float movingTurnSpeed = 360;
+		float stationaryTurnSpeed = 360;
+		float turnAmount;
+		float forwardAmount;
     // bool isInDirectMode = false;
         
     // Callback
@@ -32,31 +36,37 @@ namespace RPG.Characters
 			}
 		}
 
+    public void Move(Vector3 move)
+    {
+      SetForwardAndTurn(move);
+      ApplyExtraTurnRotation();
+      UpdateAnimator();
+    }
+
     private void Start()
     {
       CameraRaycaster cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
       animator = GetComponent<Animator>();
-      character = GetComponent<ThirdPersonCharacter>();
       rigidbody = GetComponent<Rigidbody>();
       agent = GetComponent<NavMeshAgent>();
       agent.updateRotation = false;
       agent.updatePosition = true;
       agent.stoppingDistance = stoppingDistance;
 
-      walkTarget = new GameObject("walkTarget");
-
       cameraRaycaster.onMouseOverTerrain += ProcessMouseMovement;
       cameraRaycaster.onMouseOverEnemy += ProcessEnemyInteraction;
+
+      rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
     }
 
     private void Update() {
       if (agent.remainingDistance > agent.stoppingDistance)
       {
-        character.Move(agent.desiredVelocity);
+        Move(agent.desiredVelocity);
       }
       else
       {
-        character.Move(Vector3.zero);
+        Move(Vector3.zero);
       }
     }
 
@@ -75,6 +85,29 @@ namespace RPG.Characters
         agent.SetDestination(destination);
       }
     }
+
+    private void SetForwardAndTurn(Vector3 move)
+    {
+      if (move.magnitude > moveThreshold) move.Normalize();
+      var localMove = transform.InverseTransformDirection(move);
+      turnAmount = Mathf.Atan2(localMove.x, localMove.z);
+      forwardAmount = localMove.z;
+    }
+
+    void UpdateAnimator()
+		{
+			// update the animator parameters
+			animator.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+			animator.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+      animator.speed = animSpeedMultiplier;
+		}
+
+		void ApplyExtraTurnRotation()
+		{
+			// help the character turn faster (this is in addition to root rotation in the animation)
+			float turnSpeed = Mathf.Lerp(stationaryTurnSpeed, movingTurnSpeed, forwardAmount);
+			transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+		}
 
     // void OnDrawGizmos()
     // {
