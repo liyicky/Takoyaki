6f6816989b75113bd031e40e41e7064f9ab256d1
liyicky <liyicky@gmail.com>
2019-01-25 23:36:41 +0900

    coroutines for attacking

--- Assets/_Characters/Player/PlayerControl.cs
+++ Assets/_Characters/Player/PlayerControl.cs
@@ -10,12 +10,10 @@ namespace RPG.Characters
   public class PlayerControl : MonoBehaviour
   {
     Character character;
-    CameraRaycaster cameraRaycaster;
     GameObject currentTarget;
     SpecialAbilities abilities;
     Animator animator;
     WeaponSystem weaponSystem;
-
 
     // Start is called before the first frame update
     private void Start()
@@ -46,6 +44,7 @@ namespace RPG.Characters
 
     private void RegisterForMouseEvents()
     {
+      CameraRaycaster cameraRaycaster;
       cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
       cameraRaycaster.onMouseOverEnemy += ProcessEnemyInteraction;
       cameraRaycaster.onMouseOverTerrain += ProcessTerrainInteraction;
@@ -53,16 +52,47 @@ namespace RPG.Characters
 
     private void ProcessEnemyInteraction(EnemyAI enemy)
     {
-      currentTarget = enemy.gameObject;
-      if (Input.GetMouseButtonDown(0))
+      if (Input.GetMouseButtonDown(0) && IsTargetInRange(enemy.gameObject))
       {
+        currentTarget = enemy.gameObject;
         weaponSystem.Attack(enemy.gameObject);
       }
-      else if (Input.GetMouseButtonDown(1))
+      else if (Input.GetMouseButtonDown(0) && !IsTargetInRange(enemy.gameObject))
+      {
+        StartCoroutine(MoveAndAttack(enemy));
+      }
+      else if (Input.GetMouseButtonDown(1) && IsTargetInRange(enemy.gameObject))
       {
         abilities.AttemptSpecialAbility(2, currentTarget);
       }
+      else if (Input.GetMouseButtonDown(1) && !IsTargetInRange(enemy.gameObject))
+      {
+        StartCoroutine(MoveAndPowerAttack(enemy));
+      }
     }
+
+    IEnumerator MoveAndTarget(EnemyAI target)
+    {
+      character.SetDestination(target.transform.position);
+      while (!IsTargetInRange(target.gameObject))
+      {
+        yield return new WaitForEndOfFrame();
+      }
+      yield return new WaitForEndOfFrame();
+    }
+
+    IEnumerator MoveAndAttack(EnemyAI target)
+    {
+      yield return StartCoroutine(MoveAndTarget(target));
+      weaponSystem.Attack(target.gameObject);
+    }
+
+    IEnumerator MoveAndPowerAttack(EnemyAI target)
+    {
+      yield return StartCoroutine(MoveAndTarget(target));
+      abilities.AttemptSpecialAbility(2, target.gameObject);
+    }
+    
 
     private void ProcessTerrainInteraction(Vector3 destination)
     {
@@ -70,6 +100,12 @@ namespace RPG.Characters
       {
         character.SetDestination(destination);
       }
+    }
+
+    private bool IsTargetInRange(GameObject target)
+    {
+      float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
+      return distanceToTarget <= weaponSystem.GetCurrentWeapon().AttackRadius();
     }
 
     void ScanForAbilityKeyDown()
